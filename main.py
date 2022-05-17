6ded8cbfac7678ad933418f41166002979b4df76
Will <slorkda@gmail.com>
2022-05-16 19:28:09 -0700

    Wrote prototype rendering algorithm, but there is still a bug, probably in general.cpp

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ def circle (draw_obj, location, radius):
 	l = [(location[0] - radius, location[1] - radius), (location[0] + radius, location[1] + radius)]
 	draw_obj.ellipse(l, fill = (255, 255, 255))
 
+def map (a, b, t, x, y):
+	return (t - a) / (b - a) * (y - x) + x
+
 #---------------------------------------------------------------------------------------------------
 
 print("Beginning to render %s images of %s planets. (Seed: %s)" % (len(data), \
@@ -38,6 +41,23 @@ print("Beginning to render %s images of %s planets. (Seed: %s)" % (len(data), \
 output_images = []
 count = 0
 
+loc_min_x = 0
+loc_min_y = 0
+loc_max_x = 0
+loc_max_y = 0
+
+# Find the minimum and maximum values of x and y
+for i in data:
+	for j in i:
+		if j["position"][0] > loc_max_x:
+			loc_max_x = j["position"][0]
+		elif j["position"][0] < loc_min_x:
+			loc_min_x = j["position"][0]
+		if j["position"][1] > loc_max_y:
+			loc_max_y = j["position"][1]
+		elif j["position"][1] < loc_min_y:
+			loc_min_y = j["position"][1]
+
 for i in data:
 	# remember that count refers to the index of i, and i is the data at index count
 	output_images.append(Image.new("RGB", (conf.width, conf.height)))
@@ -45,10 +65,12 @@ for i in data:
 	for j in i:
 		# Render each individual planet
 		radius = 3 # TODO
-		location = (5 * count, 5 * count)
+		location = (map(loc_min_x, loc_max_x, j["position"][0], 0, conf.width),
+					map(loc_min_y, loc_max_y, j["position"][1], 0, conf.height))
 		circle(draw, location, radius)
-	output_images[count];
+	print("Completed %s of %s." % (count + 1, len(data)))
 	count += 1
 
+print("Writing data to %s." % (output_location))
 output_images[0].save(output_location, save_all = True, append_images = output_images[1:], \
 	duration = conf.duration, loop = conf.loop)
